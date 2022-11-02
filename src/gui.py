fbf9f61c071ad1233125fee9633851b752d16155
ksklorz <kon.sklorz@gmail.com>
2022-11-02 23:03:13 +0100

    added coef setting in gui

--- src/gui.py
+++ src/gui.py
@@ -13,20 +13,33 @@ class GUI:
         self.form = tk.Tk()
         self.form.resizable(width=False, height=False)
         self.form.title("proj")
+        self.form.minsize(500,500)
 
         ################
-        self.frm_coeff = tk.Frame(master = self.form)
+        self.frm_coeff = tk.Frame(
+            master = self.form,
+            height=250)
         self.lbl_coeff = tk.Label(
             master=self.frm_coeff,
             text="coeff.:"
         )
 
+        self.sv = tk.StringVar()
+
         self.ent_coeff = tk.Entry(
             master=self.frm_coeff,
-            width=7 
+            width=10,
+            textvariable=self.sv,
         )
-        self.ent_coeff.grid(row=0,column=1, sticky='w')
-        self.lbl_coeff.grid(row=0,column=0, sticky="e")
+        self.ent_coeff.insert(0,"0.5")
+        self.sv.trace("w", lambda name, index, mode, sv=self.sv: self.ent_coeffChange(sv))
+        
+        self.ent_coeff.bind('<Return>', self.ent_coeffKey)
+
+
+
+        self.ent_coeff.grid(row=0,column=1, sticky='wn')
+        self.lbl_coeff.grid(row=0,column=0, sticky="en")
         ################
 
         self.isStab = tk.BooleanVar()
@@ -37,8 +50,8 @@ class GUI:
             command=self.chk_stabChange
         )
 
-        self.frm_coeff.grid(row=0, column=0, padx=10)
-        self.chk_stab.grid(row=1,column=0, padx=10)
+        self.frm_coeff.grid(row=0, column=0, padx=220, pady= 150)
+        self.chk_stab.grid(row=1,column=0, padx=10, pady= 0, sticky= 'n')
 
 
         self.form.mainloop()
@@ -46,5 +59,16 @@ class GUI:
     def chk_stabChange(self):
         stabCoeff.isStab = self.isStab.get()
         print(stabCoeff.isStab)
+
+    def ent_coeffChange(self, sv):
+        self.ent_coeff.config({"bg": "Yellow"})
+
+    def ent_coeffKey(self, x):
+        try:
+            stabCoeff.coeff = float(self.ent_coeff.get())
+            self.ent_coeff.config({"bg": "White"})
+        except:
+            print("conversion error")
+        
     
 
